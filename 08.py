26dbb6cf6855efa5c7c6c91691d38cddf8cdf939
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-05-05 14:47:45 +0530

    Add new logic for runner-up program

--- 08.py
+++ 08.py
@@ -1,15 +1,24 @@
-if __name__ == '___main__':
+if __name__ == '__main__':
     records = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        records.append[(name, score)]
+    user_input = int(input("Enter how many students data you want to enter: "))
+    for student in range(user_input):
+        name = input("Enter Name: ")
+        score = float(input("Enter Score: "))
+        records.append([name, score])
 
-        unique_score =  sorted(list(set(x[1] for x in records )))
+    print(f"Total List: {records}")
 
-        second_lowest_grade = unique_score[1]
+    unique_score =  sorted(list(set(x[1] for x in records )))
 
-        final_name = sorted(x[0] for x in records if x == second_lowest_grade)
+    if len(unique_score)<2:
+        print("Not enough Unique Scores to find Runner-Up")
+    else:
+        second_lowest_grade = unique_score[-2]
+
+    final_name = sorted(x[0] for x in records if x[1] == second_lowest_grade)
         
-        print(final_name)
+    print("Runner-Up details are as follows: ")
+
+    for student in final_name:
+        print(f"Student Name: {student[0]} and Student Score: {student[1]} ")
 
