97e9fc3e194401ccce241dd1938dbd38d8a29bd9
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-06-03 20:49:55 +0530

    Add basic logic for Hackerrank logo program

---
+++ 17.py
@@ -0,0 +1,9 @@
+thickness = 5 #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
