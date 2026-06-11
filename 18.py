9b5638a751854b010f9b95a43154e7802e8cd65a
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-06-11 10:58:21 +0530

    Add

--- 18.py
+++ 18.py
@@ -5,6 +5,11 @@ c = 'H'
 for i in range(thickness):
     print((c*i).rjust(thickness - 1) + c + (c*i).ljust(thickness - 1))
 
-# Top pillars
-for i in range(thickness):
+# Top Pillars
+for i in range(thickness + 1):
     print((c*thickness).center(thickness*2) + (c*thickness).center(thickness*6))
+
+# Middle Belt
+for i in range((thickness+1)//2):
+    print((c*(thickness*3)).rjust(thickness) + (c*(thickness*3)).ljust(thickness*6))
+
