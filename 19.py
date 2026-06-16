d23fb083a72367745df13f7fe74ac5213a253f09
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-06-16 15:31:18 +0530

    Add logic for Door Mat Design Program

--- 19.py
+++ 19.py
@@ -1,9 +1,32 @@
-N, M = map(int, input().split())
+if __name__ == '__main__':
+    print('\n','*'*30,"Welcome to the Program",'*'*30,'\n')
 
-for i in range(1, N, 2):
-    print((i*".|.").center(M, "-"))
+    
 
-print("Welcome".center(M, "-"))
+    while True:
+        N = int(input("Enter Odd Positive Natural Number: "))
+        print()
+        
+        if N%2 == 0 and N<0:
+            print(f"{N} is a Negative Even Number! Please try again.\n")
+        elif N%2 == 0: 
+            print(f"{N} is an Even Number! Please try again.\n")
+        elif N<0:
+            print(f"{N} is a Negative Number! Please try again.\n") 
+        elif N==0:
+            print(f"{N} is Zero! Natural Numbers should be greater than Zero.\n")    
+        else:
+            break
+    
+    M = N*3
 
-for i in range(N-2, -1, -2):
-    print(i*".|.".center(M, "-"))
+    for i in range(1, N, 2):
+        print((i*".|.").center(M, "-"))
+
+    print("Welcome".center(M, "-"))
+
+    for i in range(N-2, -1, -2):
+        print((i*".|.").center(M, "-"))
+    
+    print('\n','*'*30,"Thank You",'*'*30,'\n')
+
