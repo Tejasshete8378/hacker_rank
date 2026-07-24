d786c31920b94d23151c37fbf38c09ff28dd11be
Tejasshete8378 <Tejasshete8378@users.noreply.github.com>
2026-07-24 20:00:48 +0530

    Add basic logic for set program

---
+++ 38.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    s1 = int(input("Enter total number of Students subscibed to English Newspaper: "))
+    print()
+    english = input("Enter Name of Students subscribed to English Newspaper: ")
+    set_english = set(input(english))
