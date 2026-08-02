ff653be2eb6fe88776fb9236159cd1cc25e105fe
Tejas Shete <147392419+Tejasshete8378@users.noreply.github.com>
2026-08-02 18:45:30 +0530

    Add

--- 43.py
+++ 43.py
@@ -1,3 +1,26 @@
 if __name__ == '__main__':
-    eng_num = int(input("Enter Number of Students Subscribed to English Newspaper: "))
-    eng_roll = input("Enter Roll Number of Studens Sub")
+    print('\n','*'*30,"Welcome to the Program",'*'*30,'\n')
+    eng_num = int(input("Enter Number of Students Subscribed to English Newspaper: ").strip())
+    print()
+    eng_roll = set()
+    for i in range(eng_num):
+        eng_roll.add(input(f"Enter Roll Number of Studens No. {i+1}: ").strip())
+        print()
+
+    french_num = int(input("Enter Number of Students Subscribed to French Newspaper: ").strip())
+    print()
+    french_roll = set()
+    for i in range(french_num):
+        french_roll.add(input(f"Enter Roll Number of Studens No. {i+1}: ").strip())
+        print()
+
+    unique_roll = eng_roll.difference(french_roll)
+    total_unique = len(unique_roll)
+
+    print(f"Unique Roll Numbers are: {unique_roll}\n") 
+    print(f"Total Unique Roll Numbers are: {total_unique}")
+
+    print('\n','*'*30,"Thank You",'*'*30,'\n')
+
+
+
