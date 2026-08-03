5887928d1797b381f4f8468f9987d0a8b199f195
Tejas Shete <147392419+Tejasshete8378@users.noreply.github.com>
2026-08-03 14:22:09 +0530

    Add logic for Students subscribed for English Newspaper only

--- 43.py
+++ 43.py
@@ -1,3 +1,35 @@
+"""
+PROBLEM STATEMENT : Students Subscribed to English Newspaper Only
+---------------------------------------------------------------------------------------------
+Objective:
+--------------
+To find and display the roll numbers and total count of students who are subscribed exclusively to the English newspaper and 
+not to the French newspaper.
+
+Constraint:
+--------------
+- Roll numbers are entered as strings and stored using sets to automatically handle uniqueness.
+- Input counts for both subscriptions must be valid integers.
+
+Input:
+--------------
+- An integer representing the number of students subscribed to the English newspaper.
+- A sequence of student roll numbers subscribed to the English newspaper.
+- An integer representing the number of students subscribed to the French newspaper.
+- A sequence of student roll numbers subscribed to the French newspaper.
+
+Output:
+--------------
+- The roll numbers of students subscribed to the English newspaper only.
+- The total count of students subscribed to the English newspaper only.
+
+Task:
+--------------
+Read the roll numbers of students subscribed to English and French newspapers, determine those who take 
+only the English newspaper using set difference, and output their roll numbers along with the total count.
+"""
+
+
 if __name__ == '__main__':
     print('\n','*'*30,"Welcome to the Program",'*'*30,'\n')
     eng_num = int(input("Enter Number of Students Subscribed to English Newspaper: ").strip())
@@ -14,11 +46,11 @@ if __name__ == '__main__':
         french_roll.add(input(f"Enter Roll Number of Studens No. {i+1}: ").strip())
         print()
 
-    unique_roll = eng_roll.difference(french_roll)
-    total_unique = len(unique_roll)
+    only_eng_roll = eng_roll.difference(french_roll)
+    total_eng_roll = len(only_eng_roll)
 
-    print(f"Unique Roll Numbers are: {unique_roll}\n") 
-    print(f"Total Unique Roll Numbers are: {total_unique}")
+    print(f"Roll Numbers of Students Subscribed to English Newspaper only are: {only_eng_roll}\n") 
+    print(f"Total Number of Students Subscribed to English Newspaper only are: {total_eng_roll}")
 
     print('\n','*'*30,"Thank You",'*'*30,'\n')
 
