6ead58cbb3a36cc8e3b757b0da6de16b26483b5e
Tejas Shete <147392419+Tejasshete8378@users.noreply.github.com>
2026-08-06 12:46:03 +0530

    Add

--- 46.py
+++ 46.py
@@ -1,2 +1,13 @@
 # Set Mutations Problem on Hackerrank
+if __name__ == '__main__':
+    print('\n','*'*30,"Welcome to the Program",'*'*30,'\n')
+    total_a = int(input("Enter Total Number of Elements of Set: "))
+    set_a = set(input("Enter Elements of Set A").split())
+    other_set = int(input("Enter Number of Other Sets: "))
 
+    for i in other_set:
+        op_name = input("Enter Operation Name: ")
+        len_other_set = int(input("Enter Length of Other Set: "))
+        set_b = set(input("Enter Elements of Set B: "))
+
+    print('\n','*'*30,"Thank You",'*'*30,'\n')
